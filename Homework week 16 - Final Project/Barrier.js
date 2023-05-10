e40b315d1a5e985fe0a6487531e8884680e66784
DtCarpentry <60998459+amb1gui@users.noreply.github.com>
2023-05-10 10:09:56 -0600

    hw

---
+++ Homework week 16 - Final Project/Barrier.js
@@ -0,0 +1,16 @@
+﻿class Barrier
+{
+	//constructor
+	constructor(x, y, w, h)
+	{
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+	display()
+	{
+		fill(0, 0, 0);
+		rect(this.x, this.y, this.w, this.h);
+	}
+}
